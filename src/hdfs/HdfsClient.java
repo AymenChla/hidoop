0950273c04323f24f2f6fe297d8ca2098f9c6838
aymenchla <aymen.chla@gmail.com>
2019-03-31 19:04:04 +0200

    no block but no writing

--- src/hdfs/HdfsClient.java
+++ src/hdfs/HdfsClient.java
@@ -296,15 +296,15 @@ import formats.LineFormat;
 				List<MetadataChunk> chunks = metadataFile.getChunks();
 				for(MetadataChunk chunk : chunks)
 				{
-					boolean done = false;
-					for(int j = 0 ; j < chunk.getDatanodes().size() && !done; j++)
-					{
+					//boolean done = false;
+					//for(int j = 0 ; j < chunk.getDatanodes().size() && !done; j++)
+					//{
 						List<DataNodeInfo> datanodes = chunk.getDatanodes();
 						Socket client = null;
 						ObjectOutputStream oos =null;
 						ObjectInputStream ois = null;
 						try {
-							client = new Socket(datanodes.get(j).getIp(),datanodes.get(j).getPort());
+							client = new Socket(datanodes.get(0).getIp(),datanodes.get(0).getPort());
 							String handle = chunk.getHandle();
 							
 							String i = handle.substring(handle.length()-1,handle.length());
@@ -326,7 +326,7 @@ import formats.LineFormat;
 			    			}
 			    			
 			    			
-			    			done = true;
+			    		//	done = true;
 						} catch (IOException | ClassNotFoundException e) {
 							// TODO Auto-generated catch block
 							//e.printStackTrace();
@@ -344,7 +344,7 @@ import formats.LineFormat;
 								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
-						}
+						//}
 						
 		    		
 					}
